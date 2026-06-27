98e8f626ef0cd44efda9e5b5b778b571af80a33b
AliW030 <a.boulagrous@gmail.com>
2026-06-27 19:23:59 +0200

    Update activity name to use flag emoji

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Glory To The State Of Israel 🇮🇱",
+        name: "Glory To The State Of Israel :flag_il: ",
         // Activity type number (0 = Playing).
         type: 0,
       },
