a91572be85e6cabd04c9f6047e7219ee9b11be87
AliW030 <a.boulagrous@gmail.com>
2026-06-27 19:24:37 +0200

    Remove emoji from bot activity name

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Glory To The State Of Israel :flag_il: ",
+        name: "Glory To The State Of Israel",
         // Activity type number (0 = Playing).
         type: 0,
       },
